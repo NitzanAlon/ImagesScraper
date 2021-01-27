fbb7c60fe394679f6c35d4affcfb3e9f9d290685
Nitzan <nitzanalon90@gmail.com>
2021-01-27 17:01:15 +0200

    Refactoring and console logs with spinner

--- index.js
+++ index.js
@@ -2,63 +2,83 @@ const fs = require("fs");
 const path = require("path");
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
+const ora = require("ora");
 
 const { createB64Image, createRegularImage, isImageUrl } = require("./utils/image");
 const generateGridItem = require("./utils/dom");
 
-try {
-    const URL = process.argv[2];
-    const DEST_FOLDER = path.join(__dirname, process.argv[3]);
-    
+const URL = process.argv[2];
+const DEST_FOLDER = path.join(__dirname, process.argv[3]);
+const templateHtml = fs.readFileSync("./template.html", "utf-8");
+const $ = cheerio.load(templateHtml);
+
+try { 
     if(!fs.existsSync(DEST_FOLDER)) {
         fs.mkdirSync(DEST_FOLDER);
     }
+} catch(err) {
+    console.log(`Something went wrong! ${err.message}`);
+    return;
+}
     
-    const templateHtml = fs.readFileSync("./template.html", "utf-8");
-    const $ = cheerio.load(templateHtml);
-    
-    (async () => {
+(async () => {
+    try {
+        console.log(`Scraping images from: ${URL}`);
         var browser = await puppeteer.launch();
         const page = await browser.newPage();
         const promises = [];
+        var spinner = ora("Searching for images...").start();
+    
         page.on("response", response => {
             const url = response.url();
             const resContentType = response.headers()["content-type"];
+            // Base64 images
             if(url.indexOf(";base64,") > -1) {
+                spinner.text = "Currently scraping a base64 image";
                 promises.push(
                     createB64Image(url, DEST_FOLDER).then(imgPath => {
-                        if(imgPath && resContentType) {
-                            generateGridItem($, "container", imgPath, resContentType, url)
-                        }
+                        generateGridItem($, "container", imgPath, resContentType, url)
                     })
-                    .catch(err => console.log(err.message))
+                    .catch(err => {
+                        spinner.fail(`Unable to scrape image from: ${url}, reason: ${err.message}.`);
+                        spinner.start();
+                    })
                 );
-            } 
-            else {
-                if(response.request().resourceType() === "image" && isImageUrl(url)) {
-                    response.buffer()
-                        .then(file => {
-                            promises.push(
-                                createRegularImage(file, url, DEST_FOLDER)
-                                    .then(imgPath => {
-                                        if(imgPath && resContentType) {
-                                            generateGridItem($, "container", imgPath, resContentType, url);
-                                        }
-                                    })
-                                    .catch(err => console.log(err))
-                            );
-                        })
-                        .catch(err => console.log(err.message))
-                }
+            }
+            // Other images
+            else if(response.request().resourceType() === "image" && isImageUrl(url)) {
+                spinner.text = `Scraping image from: ${url}`;
+                response.buffer()
+                    .then(file => {
+                        promises.push(
+                            createRegularImage(file, url, DEST_FOLDER)
+                                .then(imgPath => {
+                                    generateGridItem($, "container", imgPath, resContentType, url);
+                                })
+                                .catch(err => {
+                                    spinner.fail(`Unable to scrape image from: ${url}, reason: ${err.message}.`);
+                                    spinner.start();
+                                })
+                        );
+                    })
+                    .catch(err => console.log(err.message));
             }
         });   
-
+    
         await page.goto(URL);
-        await browser.close();
+    
         Promise.allSettled(promises).then(() => {
-            fs.writeFileSync(path.join(DEST_FOLDER, "index.html"), $.html())
-        }).catch(err => console.log(err))
-    })();
-} catch(err) {
-    console.log(err);
-}
+            const htmlResult = "index.html";
+            spinner.succeed(`Done scraping! Images can be found in: ${DEST_FOLDER}`);
+            spinner.start("Creating HTML file");
+            fs.writeFileSync(path.join(DEST_FOLDER, htmlResult), $.html())
+            spinner.succeed(`HTML file created: ${path.join(DEST_FOLDER, htmlResult)}`);
+        }).catch(err => {
+            spinner.fail(`Something went wrong! ${err.message}`);
+        })
+    } catch(err) {
+        spinner.fail(`Something went wrong! ${err.message}`);
+    } finally {
+        await browser.close();
+    }
+})();
